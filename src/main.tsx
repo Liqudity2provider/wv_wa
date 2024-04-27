c9fcf3bbfd9144339035108ca7d9c6290b1ff42e
Mike Butenko <mykhailo.bytenko@k-data.llc>
2024-04-27 04:54:32 +0200

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // New import for React 18
 import Clicker from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Clicker />
   </React.StrictMode>
